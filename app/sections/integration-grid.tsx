ee2144ab058a3dd8d47e19f1cbb6558eb11dc6d0
_cession <hyperturboking-sketch@users.noreply.github.com>
2026-08-04 00:45:55 +0530

    Replace integration grid logos with real brand SVGs

--- app/sections/integration-grid.tsx
+++ app/sections/integration-grid.tsx
@@ -1,30 +1,43 @@
-import "devicons/css";
 import {
   siAutocad,
   siAutodeskrevit,
   siQuickbooks,
+  siHubspot,
+  siGoogle,
+  siBentley,
+  siSketchup,
+  siTrimble,
+  siArcgis,
+  siBim,
   type SimpleIcon,
 } from "simple-icons";
+
+const SLACK_PATH =
+  "M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.528 2.528 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zM6.313 15.165a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.528 2.528 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.522v-6.313zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.522v2.52H8.834zM8.834 6.313a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zM17.688 8.834a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V2.522A2.527 2.527 0 0 1 15.165 0a2.528 2.528 0 0 1 2.523 2.522v6.312zM15.165 18.956a2.528 2.528 0 0 1 2.523 2.522A2.528 2.528 0 0 1 15.165 24a2.527 2.527 0 0 1-2.52-2.522v-2.522h2.52zM15.165 17.688a2.527 2.527 0 0 1-2.52-2.523 2.526 2.526 0 0 1 2.52-2.52h6.313A2.527 2.527 0 0 1 24 15.165a2.528 2.528 0 0 1-2.522 2.523h-6.313z";
+
+const SALESFORCE_PATH =
+  "M10.005 5.416c.75-.796 1.845-1.306 3.046-1.306 1.56 0 2.954.9 3.689 2.205.63-.3 1.35-.45 2.101-.45 2.849 0 5.159 2.34 5.159 5.22s-2.311 5.22-5.176 5.22c-.345 0-.689-.044-1.02-.104-.645 1.17-1.875 1.95-3.3 1.95-.6 0-1.155-.15-1.65-.375-.659 1.546-2.189 2.624-3.975 2.624-1.859 0-3.45-1.169-4.05-2.819-.27.061-.54.075-.825.075-2.204 0-4.005-1.8-4.005-4.05 0-1.5.811-2.805 2.01-3.51-.255-.57-.39-1.2-.39-1.846 0-2.58 2.1-4.649 4.65-4.649 1.53 0 2.85.704 3.72 1.8";
 
 interface Integration {
   name: string;
   color: string;
-  devicon?: string;
   siIcon?: SimpleIcon;
-  letter?: string;
+  path?: string;
 }
 
 const integrations: Integration[] = [
   { name: "Revit", color: "#186BFF", siIcon: siAutodeskrevit },
-  { name: "Tekla", color: "#4DB6AC", letter: "T" },
   { name: "AutoCAD", color: "#E51050", siIcon: siAutocad },
-  { name: "Planswift", color: "#F36C21", letter: "P" },
-  { name: "Bluebeam", color: "#00A4E4", letter: "B" },
-  { name: "Slack", color: "#4A154B", devicon: "devicons-slack" },
-  { name: "Salesforce", color: "#00A1E0", devicon: "devicons-salesforce" },
-  { name: "HubSpot", color: "#FF7A59", devicon: "devicons-hubspot" },
-  { name: "Google", color: "#4285F4", devicon: "devicons-google" },
+  { name: "Bentley", color: "#3F9DBF", siIcon: siBentley },
+  { name: "SketchUp", color: "#0057F7", siIcon: siSketchup },
+  { name: "Trimble", color: "#27337B", siIcon: siTrimble },
+  { name: "ArcGIS", color: "#2C7AC3", siIcon: siArcgis },
+  { name: "BIM", color: "#0F62FE", siIcon: siBim },
+  { name: "HubSpot", color: "#FF7A59", siIcon: siHubspot },
+  { name: "Google", color: "#4285F4", siIcon: siGoogle },
   { name: "QuickBooks", color: "#2CA01C", siIcon: siQuickbooks },
+  { name: "Slack", color: "#4A154B", path: SLACK_PATH },
+  { name: "Salesforce", color: "#00A1E0", path: SALESFORCE_PATH },
 ];
 
 function IntegrationTile({ integration }: { integration: Integration }) {
@@ -39,19 +52,10 @@ function IntegrationTile({ integration }: { integration: Integration }) {
         >
           <path fill={integration.color} d={integration.siIcon.path} />
         </svg>
-      ) : integration.devicon ? (
-        <span
-          className={`devicons text-[22px] ${integration.devicon}`}
-          style={{ color: integration.color }}
-          aria-hidden="true"
-        />
       ) : (
-        <span
-          className="flex size-6 shrink-0 items-center justify-center rounded-[8px] text-[12px] font-semibold text-white"
-          style={{ backgroundColor: integration.color }}
-        >
-          {integration.letter}
-        </span>
+        <svg viewBox="0 0 24 24" role="img" className="size-6 shrink-0">
+          <path fill={integration.color} d={integration.path} />
+        </svg>
       )}
       <span className="text-[14px] font-medium whitespace-nowrap text-[#0F172A]">
         {integration.name}
